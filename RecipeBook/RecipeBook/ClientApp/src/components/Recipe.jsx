206eff52b18c5f4e795b956a95c11ec648ce7908
Rostyslav Skrypnyk <skrypnik-r@ukr.net>
2019-07-09 21:15:01 +0300

    changes to get data from api

--- RecipeBook/RecipeBook/ClientApp/src/components/Recipe.jsx
+++ RecipeBook/RecipeBook/ClientApp/src/components/Recipe.jsx
@@ -4,12 +4,12 @@ import ReactDOM from "react-dom";
 class Recipe extends React.Component {
     state = { name: "", steps: [], ingredients: [] };
     onNameChange(e) {
-		this.setState({
-			name: e.target.value
-		});
+        this.setState({
+            name: e.target.value
+        });
 
     }
-   
+
     onStepsChange(e) {
         this.setState({
             steps: e.target.value
@@ -21,31 +21,30 @@ class Recipe extends React.Component {
         });
     }
 
-    
-  render()
-  {
-      const[name]=this.state;
-      const steps=this.state.steps.map(step=><li>{step}</li>)
-      const ingredients=this.state.ingredients.map(ingredient=><li>{ingredient}</li>)
-      return(
-<div>   
-    <div>{name}</div>
-    <ul>{steps}</ul>
-    <ul>{ingredients}</ul>
-    <div>
-        <button onClick={this.props.editRecipe}></button>
-        <button onClick={this.props.deleteRecipe}></button>
-    </div>
-<div>
-<ul>
 
-</ul>
+    render() {
+        const {name} = this.state;
+        const steps = this.state.steps.map(step => <li>{step}</li>)
+        const ingredients = this.state.ingredients.map(ingredient => <li>{ingredient}</li>)
+        return (
+            <div>
+                <div>{name}</div>
+                <ul>{steps}</ul>
+                <ul>{ingredients}</ul>
+                <div>
+                    <button onClick={this.props.editRecipe}></button>
+                    <button onClick={this.props.deleteRecipe}></button>
+                </div>
+                <div>
+                    <ul>
 
-</div>
-</div>
+                    </ul>
 
-      );
-  }
+                </div>
+            </div>
+
+        );
+    }
 
 }
 
--- RecipeBook/RecipeBook/ClientApp/src/index.js
+++ RecipeBook/RecipeBook/ClientApp/src/index.js
@@ -1,83 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import "./styles.css"
-import Recipe from "./components/Recipe"
-import Footer from "./components/Footer"
-import Header from "./components/Header"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import Recipe from "./components/Recipe";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 class App extends React.Component {
-    state={recipes:[], arrLength:0, isHidden:true}
+  state = { recipes: [], arrLength: 0, isHidden: true };
 
-
-    componentDidMount()
-    {
+  componentDidMount() {
     fetch("https://localhost:44314/api/recipe")
-    .then(res=>res.json()).then(res=>{
-       JSON.stringify(res);
-       console.log(res);
-      this.setState({recipes:res});
-     })
-     console.log("recipe");
-    }
-    editRecipe=e=>
-    {
-      
-    }
+      .then(res => res.json())
+      .then(res => {
+        JSON.stringify(res);
+        console.log(res);
+        this.setState({ recipes: res });
+      });
+    console.log("recipe");
+  }
+  editRecipe = e => {};
 
-    createRecipe=()=>
-    {
-        this.setState({isHidden :false});
-    }
-    addRecipe=e=>
-    {
-       
+  createRecipe = () => {
+    this.setState({ isHidden: false });
+  };
+  addRecipe = e => {};
 
-    }
+  deleteRecipe = e => {};
+  render() {
+    const recipes = this.state.recipes.map((recipe, idx) => {
+      return (
+        <Recipe
+          recipe={recipe}
+          id={idx + 1}
+          deleteRecipe={this.deleteRecipe}
+          editRecipe={this.editRecipe}
+        />
+      );
+    });
+    return (
+      <div>
+        <div className="App">
+          <Header />
+          <button onClick={this.createRecipe}>Add recipe</button>
+          <div>{recipes}</div>
+        </div>
 
-    deleteRecipe=e=>
-    {
+        <div
+          id="newRecipe"
+          class={this.state.isHidden ? "Visible" : "InVisible"}
+        >
+          <h4>Add New Recipe</h4>
+          <div>
+            <input id="add_name" type="text" />
+            <label>Name</label>
+          </div>
+          <div>
+            <input id="add_ingredients" type="text" />
+            <label htmlFor="add_ingredients"> Ingredients</label>
+          </div>
+          <div>
+            <input id="add_steps" type="text" />
+            <label> Steps </label>
+          </div>
 
-    }
-    render() {
-        const recipes= this.state.recipes.map((recipe, idx) => {
-            return <Recipe  recipe={recipe} id={idx + 1} deleteRecipe = {this.deleteRecipe} editRecipe={this.editRecipe} />
-        });;
-        return (
-            <div>
-            <div className="App">
-                <Header />
-                <button onClick={this.createRecipe}>Add recipe</button>
-                <div>
-                    {recipes}
-                </div>
-                
-            </div>
+          <div>
+            <button onClick={this.addRecipe}>Confirm</button>
+          </div>
+        </div>
 
-            <div id="newRecipe" class={this.state.isHidden?"Visible":"InVisible"}>         
-              <h4>Add New Recipe</h4>
-                <div >
-                  <input id="add_name" type="text" />
-                  <label >Name</label>
-                </div>
-                <div >
-                  <input id="add_ingredients" type="text" />
-                  <label htmlFor="add_ingredients"> Ingredients</label>
-                </div>
-                    <div >
-                  <input id="add_steps" type="text" />
-                  <label> Steps </label>
-                </div>
-             
-             <div >
-              <button onClick={this.addRecipe}  >Confirm</button>
-            </div>          
-            </div>
-
-            <Footer text="user"/> 
-            </div>
-            
-        );
-    }
+        <Footer text="user" />
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
